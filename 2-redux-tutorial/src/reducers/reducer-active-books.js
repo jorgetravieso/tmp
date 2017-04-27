798710c7db98bd5e6eccc96c615d0368ee2222fe
Jorge Travieso <jtrav029@fiu.edu>
2017-04-26 23:24:12 -0400

    Revert "adding react tutorials code"
    This reverts commit c010c8eef6a5a409a94dfdc2f1b49dfef80a6bf5.

--- 2-redux-tutorial/src/reducers/reducer-active-books.js
+++
@@ -1,8 +0,0 @@
-//State argument is not application state, only the state this reducer is responsible
-export default function (state = null, action) {
-    console.log('action', action);
-    switch (action.type) {
-        case 'BOOK_SELECTED': return action.payload;
-    }
-    return state;
-}
--- 2-redux-tutorial/src/reducers/reducer-books.js
+++
@@ -1,7 +0,0 @@
-export default function () {
-    return [
-        {  title: 'Harry Potter', pages: 101 },
-        {  title: 'JavaScript. The Good Parts', pages: 20  },
-        {  title: 'The dark tower', pages: 3020  }
-    ]
-}
